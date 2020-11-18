bb6fa562683a1fbad1520b4a8458e43b459eeb9f
Myint Myat Thein <myintmyatthein404@gmail.com>
2020-11-18 14:57:44 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from "react";
+import { FacebookShareButton, FacebookIcon} from 'react-share';
+import {Helmet} from 'react-helmet';
+class App extends Component {
+   render() {
+   return (
+    <div>
+      <Helmet>
+        <title>SIIT</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="csrf_token" content="" />
+        <meta property="type" content="website" />
+        <meta property="url" content={"currentUrl"} />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="_token" content="" />
+        <meta name="robots" content="noodp" />
+        <meta property="title" content={"title"} />
+        <meta property="quote" content={"quote"} />           {/*Optional*/}
+        <meta name="description" content={"description"} />
+        <meta property="image" content={"image"} />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={"title"} />
+        <meta property="og:quote" content={"quote"} />       {/*Optional*/}
+        <meta property="og:hashtag" content={"hashtag"} />    {/*Optional*/}
+        <meta property="og:image" content={"image"} />
+        <meta content="image/*" property="og:image:type" />
+        <meta property="og:url" content={"currentUrl"} />
+        <meta property="og:site_name" content="CampersTribe" />
+        <meta property="og:description" content={"description"} />
+      </Helmet>
+        <FacebookShareButton
+          url={"https://shaniit.org"}
         >
-          Learn React
-        </a>
-      </header>
+          <FacebookIcon/>
+        </FacebookShareButton>
     </div>
-  );
+  )};
 }
-
 export default App;
